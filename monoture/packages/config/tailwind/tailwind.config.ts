4e8b445c5cc30ead0a4186849bd3b7a6b0f1e427
sub0ne1 <iamsubhajit.d@gmail.com>
2024-01-21 11:14:51 -0500

    few more changes

---
+++ monoture/packages/config/tailwind/tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+}
