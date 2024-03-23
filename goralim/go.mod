0f4370e325e9e6f09605e8da1310edfe709aae2a
sub0ne1 <iamsubhajit.d@gmail.com>
2024-03-23 01:45:19 -0400

    Touched Rust

---
+++ goralim/go.mod
@@ -0,0 +1,5 @@
+module goralima
+
+go 1.22.0
+
+require github.com/0verread/goralim v0.0.0-20240314064256-efed567f9b70 // indirect
---
+++ learn-rust/main.rs
@@ -0,0 +1,12 @@
+fn dosomething() -> i32 {
+    return 4;
+}
+fn main() {
+    println!("hello >5{name}", name="Subhajit");
+    println!("1+2 = {sum}", sum=1+2);
+    let tt = ("hello", 776);
+    println!("{0} mr {1}", tt.0, tt.1);
+    println!("dosomething value: {}", dosomething());
+}
+
+
